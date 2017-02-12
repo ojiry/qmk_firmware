6f877b5ed8955193081657b0c12e32cab056a16c
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-02-12 12:12:42 +0900

    Change KC_LEFT to KC_LALT

--- keyboards/ergodox/ez/keymaps/ojiry/keymap.c
+++ keyboards/ergodox/ez/keymaps/ojiry/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left |  Cmd |                                       |  Cmd | Down |   [  |   ]  |   \  |
+ *   |Grv/L1|  '"  |AltShf|  Alt |  Cmd |                                       |  Cmd | Down |   [  |   ]  |   \  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,          KC_Q,    KC_W,          KC_E,    KC_R,            KC_T, TG(SYMB),
         KC_LCTRL,        KC_A,    KC_S,          KC_D,    KC_F,            KC_G,
         KC_LSFT,         KC_Z,    KC_X,          KC_C,    KC_V,            KC_B, ALL_T(KC_NO),
-        LT(SYMB,KC_GRV), KC_QUOT, LALT(KC_LSFT), KC_LEFT, GUI_T(KC_LANG2),
+        LT(SYMB,KC_GRV), KC_QUOT, LALT(KC_LSFT), KC_LALT, GUI_T(KC_LANG2),
                                                           ALT_T(KC_APP),   KC_LGUI,
                                                                            KC_HOME,
                                                  KC_SPC,  KC_BSPC,         KC_END,
