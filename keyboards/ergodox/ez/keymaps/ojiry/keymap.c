7dbbf0a6663f6d19f39b597487e118ef858235c0
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-02-12 12:04:24 +0900

    Fix format

--- keyboards/ergodox/ez/keymaps/ojiry/keymap.c
+++ keyboards/ergodox/ez/keymaps/ojiry/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1|  '"  |AltShf| Left |  Cmd |                                       |  Cmd | Down |   [  |   ]  |   \  |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
+ *                                        ,-------------.       ,---------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
@@ -40,23 +40,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,GUI_T(KC_LANG2),
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
+        KC_GRV,          KC_1,    KC_2,          KC_3,    KC_4,            KC_5, KC_LEFT,
+        KC_TAB,          KC_Q,    KC_W,          KC_E,    KC_R,            KC_T, TG(SYMB),
+        KC_LCTRL,        KC_A,    KC_S,          KC_D,    KC_F,            KC_G,
+        KC_LSFT,         KC_Z,    KC_X,          KC_C,    KC_V,            KC_B, ALL_T(KC_NO),
+        LT(SYMB,KC_GRV), KC_QUOT, LALT(KC_LSFT), KC_LEFT, GUI_T(KC_LANG2),
+                                                          ALT_T(KC_APP),   KC_LGUI,
+                                                                           KC_HOME,
+                                                 KC_SPC,  KC_BSPC,         KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_EQL,
-                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                  GUI_T(KC_LANG1), KC_DOWN,KC_LBRC,KC_RBRC,          KC_BSLS,
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_ENT, KC_SPC
+        KC_RGHT,      KC_6,          KC_7,            KC_8,    KC_9,    KC_0,    KC_MINS,
+        TG(SYMB),     KC_Y,          KC_U,            KC_I,    KC_O,    KC_P,    KC_EQL,
+                      KC_H,          KC_J,            KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        MEH_T(KC_NO), KC_N,          KC_M,            KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                     GUI_T(KC_LANG1), KC_DOWN, KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_LALT,      CTL_T(KC_ESC),
+        KC_PGUP,
+        KC_PGDN,      KC_ENT,        KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
