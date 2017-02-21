7881de8862ee03ad45de747c4cbb13dcd50fe477
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-02-21 12:56:12 +0900

    Update a lot of keymap

--- keyboards/ergodox/ez/keymaps/ojiry/keymap.c
+++ keyboards/ergodox/ez/keymaps/ojiry/keymap.c
@@ -22,14 +22,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|  `   |           |   \  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  |  L2  |  L3  |  Alt |  Cmd |                                       |  Cmd |VolUp |VolDn | Mute | Sleep|
+ *   |      |      |      |  Alt |  Cmd |                                       |  Cmd |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | PPage| LTab |       | RTab |NPage |
+ *                                        | LTab |PPage |       | NPage| RTab |
  *                                 ,------|------|------|       |------+------+------.
  *                                 | Cmd/ |  L2/ | Home |       | PgUp |Shift/|Ctrl/ |
  *                                 |Backsp|Delete|------|       |------|Enter |Space |
@@ -40,23 +40,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,              KC_1,            KC_2,            KC_3,    KC_4,            KC_5,             KC_NO,
-        KC_TAB,              KC_Q,            KC_W,            KC_E,    KC_R,            KC_T,             KC_LBRC,
-        KC_LCTL,             KC_A,            KC_S,            KC_D,    KC_F,            KC_G,
-        KC_LSFT,             KC_Z,            KC_X,            KC_C,    KC_V,            KC_B,             KC_GRV,
-        DF(BASE),            DF(SYMB),        DF(MDIA),        KC_LALT, GUI_T(KC_LANG2),
-                                                                                         LGUI(KC_LBRC),    LALT(LGUI(KC_LEFT)),
-                                                                                                           KC_HOME,
-                                                                        GUI_T(KC_BSPC),  LT(SYMB, KC_DEL), KC_END,
+        KC_ESC,        KC_1,                KC_2,            KC_3,    KC_4,            KC_5,                KC_NO,
+        KC_TAB,        KC_Q,                KC_W,            KC_E,    KC_R,            KC_T,                KC_LBRC,
+        CTL_T(KC_ESC), KC_A,                KC_S,            KC_D,    KC_F,            KC_G,
+        KC_LSFT,       KC_Z,                KC_X,            KC_C,    KC_V,            KC_B,                KC_GRV,
+        KC_NO,         KC_NO,               KC_NO,           KC_LALT, GUI_T(KC_LANG2),
+                                                                                       LALT(LGUI(KC_LEFT)), LGUI(KC_LBRC),
+                                                                                                            KC_HOME,
+                                                                      GUI_T(KC_BSPC),  LT(SYMB, KC_DEL),    KC_END,
         // right hand
-        KC_NO,               KC_6,            KC_7,            KC_8,    KC_9,            KC_0,             KC_MINS,
-        KC_RBRC,             KC_Y,            KC_U,            KC_I,    KC_O,            KC_P,             KC_EQL,
-                             KC_H,            KC_J,            KC_K,    KC_L,            KC_SCLN,          KC_QUOT,
-        KC_BSLS,             KC_N,            KC_M,            KC_COMM, KC_DOT,          KC_SLSH,          KC_RSFT,
-                                              GUI_T(KC_LANG1), KC_VOLU, KC_VOLD,         KC_MUTE,          KC_SLEP,
-        LALT(LGUI(KC_RGHT)), LGUI(KC_RBRC),
+        KC_NO,         KC_6,                KC_7,            KC_8,    KC_9,            KC_0,                KC_MINS,
+        KC_RBRC,       KC_Y,                KC_U,            KC_I,    KC_O,            KC_P,                KC_EQL,
+                       KC_H,                KC_J,            KC_K,    KC_L,            KC_SCLN,             KC_QUOT,
+        KC_BSLS,       KC_N,                KC_M,            KC_COMM, KC_DOT,          KC_SLSH,             KC_RSFT,
+                                            GUI_T(KC_LANG1), KC_NO,   KC_NO,           KC_NO,               KC_NO,
+        LGUI(KC_RBRC), LALT(LGUI(KC_RGHT)),
         KC_PGUP,
-        KC_PGDN,             CTL_T(KC_ENT),   SFT_T(KC_SPC)
+        KC_PGDN,       CTL_T(KC_ENT),       SFT_T(KC_SPC)
     ),
 /* Keymap 1: Symbol Layer
  *
